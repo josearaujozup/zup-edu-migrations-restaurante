33c4cab5ed6aec2dafae9a494411fed3703ce814
Denes <jose.araujo@zup.com.br>
2022-05-09 08:38:03 -0300

    commit inicial

---
+++ src/main/java/br/com/zup/edu/migrationrestaurante/MigrationrestauranteApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.migrationrestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationrestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationrestauranteApplication.class, args);
+	}
+
+}
